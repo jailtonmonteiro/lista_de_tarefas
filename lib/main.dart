a286c6a690ee1f5798982ce235b27c4c06548aed
Jailton Monteiro <jailtonmonteiro@monteiro.com.br>
2020-10-28 20:00:50 -0300

    Creation of file read and write functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(
@@ -12,6 +17,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _toDoList = [];
+
+  //Functions
+  //returns used file
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  //save file
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
