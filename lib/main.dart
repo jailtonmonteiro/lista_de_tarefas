82cdfa3df4fd4c1f13697a51a82570668c39d6a8
Jailton Monteiro <jailtonmonteiro@monteiro.com.br>
2020-10-28 20:46:36 -0300

    Layout creation

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List _toDoList = [];
 
-  //Functions
   //returns used file
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
@@ -33,6 +32,7 @@ class _HomeState extends State<Home> {
     return file.writeAsString(data);
   }
 
+  //Reading file
   Future<String> _readData() async {
     try {
       final file = await _getFile();
@@ -44,6 +44,51 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blue,
+        centerTitle: false,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.blue)),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.blue,
+                  child: Text("Adicionar"),
+                  textColor: Colors.white,
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+          Expanded(
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: (context, index) {
+                    return CheckboxListTile(
+                      title: Text(_toDoList[index]),
+                      value: _toDoList[index]["done"],
+                      secondary: CircleAvatar(
+                        child: Icon(_toDoList[index]["title"]
+                            ? Icons.check
+                            : Icons.error),
+                      ),
+                    );
+                  }))
+        ],
+      ),
+    );
   }
 }
